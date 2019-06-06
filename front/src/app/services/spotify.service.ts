3f74b6724e8d5f819bb0af840c8491c0fe1f32b5
unknown <jesusreyesreal@gmail.com>
2019-06-06 21:10:24 +0200

    merge

--- front/src/app/services/spotify.service.ts
+++ front/src/app/services/spotify.service.ts
@@ -4,6 +4,18 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 //importar map reactive extentions
 import { map } from "rxjs/operators";
 
+
+
+
+
+
+
+
+
+
+
+
+
 // Por lo general cuando se trabaja con API
 // Es necesario Centralizar la Informacion por eso este Service
 
@@ -16,6 +28,31 @@ export class SpotifyService {
     // console.log('Spotify Service Listo');
   }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   // Para  consulta generica
   getQuery(query: string) {
     const url = `https://api.spotify.com/v1/${query}`;
@@ -24,7 +61,7 @@ export class SpotifyService {
     // Defino Headers que API de Spotify Necesita
     const headers = new HttpHeaders({
       Authorization:
-        "Bearer BQArVcXbCiWwm4MDBs4FMBt4adl3B51ZLsGPPSECPgwYZjoN0ErLHUGz8f-lwLwbzHxTbLiG5YoswbYnClk"
+        "Bearer BQDgJjcyO84DlYg43RyHPsd3vWmvrc5yMJytdXQ_vD2EIEDbDuksSSBcgesl7BIJTGu533V5ZyVuD6t4R4w"
     });
 
     return this.http.get(url, { headers });
